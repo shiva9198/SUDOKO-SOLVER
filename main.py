fc018d43762d995d997bd4af99fc3510c09ac6a1
shiva9198 <149881931+shiva9198@users.noreply.github.com>
2024-04-14 18:00:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def is_valid(board, row, col, num):
             return False
 
     # Check 3x3 subgrid
-    start_row= row // 3
-    start_col = col // 3
+    start_row= 3 * (row // 3)
+    start_col =3 *(col // 3)
     for i in range(start_row, start_row + 3):
         for j in range(start_col, start_col + 3):
             if board[i][j] == num:
@@ -31,15 +31,15 @@ def solve_sudoku(board):
 
 # Example Sudoku board (0 represents empty cells)
 sudoku_board = [
-   [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+   [3, 0, 6, 5, 0, 8, 4, 0, 0],
+		[5, 2, 0, 0, 0, 0, 0, 0, 0],
+		[0, 8, 7, 0, 0, 0, 0, 3, 1],
+		[0, 0, 3, 0, 1, 0, 0, 8, 0],
+		[9, 0, 0, 8, 6, 3, 0, 0, 5],
+		[0, 5, 0, 0, 9, 0, 6, 0, 0],
+		[1, 3, 0, 0, 0, 0, 2, 5, 0],
+		[0, 0, 0, 0, 0, 0, 0, 7, 4],
+		[0, 0, 5, 2, 0, 6, 3, 0, 0]
 ]
 
 # Solve the Sudoku
